11600fd3bfdc2e9deb4253f49eda45c76c5114c0
carloicorcuera <carloicorcuera@gmail.com>
2024-09-19 02:20:16 +0800

    1.3.4.2

--- src/components/Base/NavBottom.js
+++ src/components/Base/NavBottom.js
@@ -10,8 +10,8 @@ function NavBottom() {
   const isLoggedIn = !!localStorage.getItem('bb_session_token'); 
   const navigate = useNavigate(); 
 
-  const [ userInfo, setUserInfo ] = useState(null);
-  const [ loading, setLoading ] = useState(true);
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserDetails = async () => {
@@ -21,24 +21,31 @@ function NavBottom() {
           setUserInfo(result);
         }
       } catch (error) {
+        // You might want to handle errors here, e.g., showing a notification
         // console.error('Error fetching user details:', error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchUserDetails();
-  }, []);
+    if (isLoggedIn) {
+      fetchUserDetails();
+    } else {
+      setLoading(false); // Ensure loading is false if not logged in
+    }
+  }, [isLoggedIn]);
 
   const handleLogout = () => {
     localStorage.removeItem('bb_session_token');
-    setTimeout(() => {
-      navigate('/home');
-    }, 100);
+    navigate('/home');
   };
 
-  if (!isLoggedIn || loading) {
-    return null; // Don't render anything if not logged in or loading
+  if (loading) {
+    return <div>Loading...</div>; // Optional: Add a loading spinner or message
+  }
+
+  if (!isLoggedIn) {
+    return null; // Optionally, render a message or redirect if not logged in
   }
 
   if (userInfo?.isSysAd) {
@@ -50,7 +57,6 @@ function NavBottom() {
   }
 
   return (
-
     <div className="nav-container-bottom">
       <p className="nav-item roboto-light">
         <Link to="/" className="nav-link" onClick={handleLogout}>Logout</Link>
